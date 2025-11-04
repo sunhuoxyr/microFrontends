9b29315ac2fff67be2fab0fc9185c4fd1f9b91c2
sunhuoxyr <sunhuoxyr@163.com>
2025-11-04 15:15:42 +0800

    feat: iframe 实现微前端

---
+++ main-server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import ejs from 'ejs';
+import config from './config.js';
+import path from 'node:path';
+
+const { port, host, __dirname } = config;
+
+const app = express();
+app.engine(".html", ejs.__express);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+app.get('/', function (req, res) {
+  res.render('main', {
+    iframeUrl: `http://${host}:${port.micro}`,
+  });
+});
+app.listen(port.main, host);
+console.log(`server start at http://${host}:${port.main}/`);
